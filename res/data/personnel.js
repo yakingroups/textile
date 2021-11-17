e78a77946188c7f3236dd6ec17295e760178fdd8
yakingroups <cvmurbaut@protonmail.com>
2021-11-17 23:23:43 +0700

    init

---
+++ res/data/personnel.js
@@ -0,0 +1,230 @@
+personnels = {
+    0: {
+        "name": "Achmad Torikul Huda",
+        "image": "res/images/personnels/torik.jpeg",
+        "spacializations": ["Web Programming", "Microcontroller", "OpenCV"],
+        "experiences":[
+            {
+                "type": "Internship",
+                "section": "Pendidikan Ahli Pemrograman Sistem Informasi",
+                "location": "PAPSI-ITS",
+                "year": 2017
+            },
+            {
+                "type": "Internship",
+                "section": "Web Development",
+                "location": "-",
+                "year": 2020
+            },
+            {
+                "type": "Internship",
+                "section": "Project Development",
+                "location": "PT.Sinergi Informatika Semen Indonesia",
+                "year": 2021
+            }
+        ],
+        "links":{
+            "github": "https://github.com/achmadthorik"
+        },
+        "contact": {
+            "email": "achmadthorik2@gmail.com"
+        },
+        "certificates": [
+            "res/images/certificates/torik/2.jpg", "res/images/certificates/torik/3.png", "res/images/certificates/torik/1.png", "res/images/certificates/torik/5.png", "res/images/certificates/torik/4.png", "res/images/certificates/torik/6.png"
+        ]
+    },
+    1:{
+        "name": "Dany Razzaq Farino",
+        "image": "res/images/personnels/bottom.jpg",
+        "spacializations": ["Web Programming", "Desktop Programming", "Python", "Tensorflow"],
+        "experiences":[
+            {
+                "type": "Internship",
+                "section": "Web Development",
+                "location": "PT. Jawa Pos Group IT",
+                "year": 2017
+            },
+            {
+                "type": "Internship",
+                "section": "Internal Web Development",
+                "location": "PT. Jawa Pos Group Media",
+                "year": 2021
+            }
+        ],
+        "links":{
+            "github": "https://github.com/admiralpuni",
+            "portofolio": "https://admiralpuni.github.io/portofolio/"
+        },
+        "contact": {
+            "email": "drfredmagic@gmail.com"
+        },
+        "certificates": [
+            "res/images/certificates/drf/1.png", "res/images/certificates/drf/2.png", "res/images/certificates/drf/3.png", "res/images/certificates/drf/4.JPG"
+        ]
+    },
+    2:{
+        "name": "Ahmad Nasrudin",
+        "image": "res/images/personnels/an.jpeg",
+        "spacializations": ["PLC Systems", "Mechanical Systems", "Graphic Design"],
+        "experiences":[
+            {
+                "type": "Internship",
+                "section": "IT Division",
+                "location": "PT. Sari Warna Asli Garment, Surakarta",
+                "year": 2017
+            },
+            {
+                "type": "Internship",
+                "section": "Robotics",
+                "location": "Robo Edu Maland",
+                "year": 2020
+            }
+        ],
+        "links":{},
+        "contact": {
+            "email": "ahmadnasrudin0723@gmail.com",
+            "phone": "089696979863"
+        },
+        "certificates": [
+            ""
+        ]
+    },
+    3:{
+        "name": "Bagas Kurniawan",
+        "image": "res/images/personnels/bk.jpeg",
+        "spacializations": ["Electrical Installation", "Electrical Control", "Electrical Inspection", "Electrical Instrument"],
+        "experiences":[
+            {
+                "type": "Internship",
+                "section": "Electrical Instalation and equipment",
+                "location": "PT Madukismo",
+                "year": 2017
+            },
+            {
+                "type": "Internship",
+                "section": "Panel Maker and Programmer PLC control",
+                "location": "PT Laksana Daya Prima Sidoarjo",
+                "year": 2021
+            }
+        ],
+        "links":{},
+        "contact": {
+            "email": "bagaskurniawan075@gmail.com",
+            "phone": "0895396651151"
+        },
+        "certificates": [
+            "res/images/certificates/bk/1.jpeg","res/images/certificates/bk/2.jpeg","res/images/certificates/bk/3.jpeg"
+        ]
+    }
+    ,
+    4:{
+        "name": "Prishandy Hamani A.",
+        "image": "res/images/personnels/pma.jpeg",
+        "spacializations": ["Electrical Systems", "Microcontroller", "Robotics", "IoT Systems"],
+        "experiences":[
+            {
+                "type": "Competition",
+                "section": "Industrial Control",
+                "location": "LKS Jawa Timur",
+                "year": 2017
+            },
+            {
+                "type": "Competition",
+                "section": "Robotics",
+                "location": "Lomba Robot Indonesia Tingkat Nasional",
+                "year": 2021
+            }
+        ],
+        "links":{},
+        "contact": {
+            "email": "prishandyhamami@gmail.com"
+        },
+        "certificates": [
+            "res/images/certificates/pma/fpcPA-1103181031-Prishandy Hamami Amrulloh - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Sertifikat Peserta - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Sertifikat_UKK_Belakang - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/IMG_0803 - 031_Prishandy Hamami.JPG", "res/images/certificates/pma/LKS Kabupaten - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Prishandy Hamami Amrulloh (juara 2 regional) - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Prishandy Hamami Amrulloh (juara 1 nasional) - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Prishandy Hamami Amrulloh (design terbaik regional) - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Sertifikat_UKK - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Prishandy Hamami Amrulloh (design terbaik nasional) - 031_Prishandy Hamami.jpg"
+        ]
+    }
+    ,
+    5:{
+        "name": "Didi Alfandi",
+        "image": "res/images/personnels/da.jpeg",
+        "spacializations": ["Embedded Systems", "Hardware", "Robotics"],
+        "experiences":[
+            {
+                "type": "Competition",
+                "section": "Robotics",
+                "location": "PENS Divisi KRSBI-Beroda",
+                "year": 2021
+            },
+            {
+                "type": "Competition",
+                "section": "Industrial Control",
+                "location": "Workshop Roof-inno Surabaya",
+                "year": 2017
+            },
+            {
+                "type": "Competition",
+                "section": "Robotics",
+                "location": "PT. Lentera Bumi Nusantara",
+                "year": 2020
+            }
+        ],
+        "links":{
+            "github": "https://github.com/dialfandi"
+        },
+        "contact": {
+            "email": "dialfandi25@gmail.com",
+            "phone": "085649070531"
+        },
+        "certificates": [
+            "res/images/certificates/da/1.jpg","res/images/certificates/da/2.jpg","res/images/certificates/da/3.jpg"
+        ]
+    }
+    ,
+    6:{
+        "name": "Alif Dhyo S.A.",
+        "image": "res/images/personnels/ad.jpeg",
+        "spacializations": ["Architectural Design", "Interior Design", "Exterior Design", "Landscape Design"],
+        "experiences":[
+            {
+                "type": "Competition",
+                "section": "Architecture",
+                "location": "CV. JEEM CONSULTAN",
+                "year": 2017
+            }
+        ],
+        "links":{
+        },
+        "contact": {
+            "email": "alfdbm.99@gmail.com",
+            "phone": "087758533671"
+        },
+        "certificates": [""]
+    }
+    ,
+    7:{
+        "name": "Achmad Fauzi N. H.",
+        "image": "res/images/personnels/af.jpeg",
+        "spacializations": ["Electrical Installation", "Electrical Control", "Electrical Maintenance", "Electrical Assembly"],
+        "experiences":[
+            {
+                "type": "Competition",
+                "section": "Electrical Installation",
+                "location": "PT. Duta Alam Persada",
+                "year": 2020
+            },
+            {
+                "type": "Competition",
+                "section": "Electrical Assembly",
+                "location": "UD. BESONIC",
+                "year": 2017
+            }
+        ],
+        "links":{
+        },
+        "contact": {
+            "email": "achmadfauzinoorhanani@gmail.com",
+            "phone": "081332678696"
+        },
+        "certificates": ["res/images/certificates/af/1.jpeg","res/images/certificates/af/2.jpeg","res/images/certificates/af/3.jpeg"]
+    }
+}
